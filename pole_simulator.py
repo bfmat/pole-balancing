b284d1ae2e272adb1d766ddfa8b7bd6f995b3e0d
brendon-ai <brendon-m@outlook.com>
2018-11-02 14:26:50 -0400

    Stuff

--- pole_simulator.py
+++ pole_simulator.py
@@ -29,39 +29,48 @@ def get_pos_accel(angle, angle_speed, angle_accel, force):
     )
 
 
-x = np.zeros((100, 100))
-for p in range(100):
-    for d in range(100):
-        angle = 0.01
+log = False
+
+p_range = 20
+d_range = 100
+target_angle = 0.1
+x = []
+for p in range(p_range):
+    for d in range(d_range):
+        angle = -0.1
         angle_speed = 0
         pos = 0
         pos_speed = 0
         force = 0
         time = 0
-        for _ in range(20000):
-            force = angle * p + angle_speed * d
+        for _ in range(2000):
+            p_error = angle - target_angle
+            d_error = angle_speed if angle > target_angle or angle < 0 else -angle_speed
+            force = (p_error * p) + (d_error * d)
             angle_accel = get_angle_accel(angle, angle_speed, force)
             pos_accel = get_pos_accel(angle, angle_speed, angle_accel, force)
             angle += angle_speed * time_step
             angle_speed += (angle_accel * time_step)
             pos += pos_speed * time_step
             pos_speed += pos_accel * time_step
-            # print('Time:', time)
-            # print('Force:', force)
-            # print('Position:', pos)
-            # print('Position Speed:', pos_speed)
-            # print('Position Acceleration:', pos_accel)
-            # print('Angle:', angle)
-            # print('Angle Speed:', angle_speed)
-            # print('Angle Acceleration:', angle_accel)
-            # print()
-            if abs(angle) > failure_angle or abs(pos) > track_limit:
-                # print('Failure')
-                x[p, d] = time
+            if log:
+                print('Time:', time)
+                print('Force:', force)
+                print('Position:', pos)
+                print('Position Speed:', pos_speed)
+                print('Position Acceleration:', pos_accel)
+                print('Angle:', angle)
+                print('Angle Speed:', angle_speed)
+                print('Angle Acceleration:', angle_accel)
+                print()
+            if abs(angle) > failure_angle:  # or abs(pos) > track_limit:
+                if log:
+                    print('Failure')
+                x.append(time)
                 break
             time += time_step
 
-print(np.unravel_index(np.argmax(x), x.shape))
+print(np.unravel_index(np.argmax(x), (p_range, d_range)))
 print(np.amax(x))
 # plt.plot(x)
 # plt.show()
