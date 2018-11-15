5a5b5a6b7752b93cab25cdc85a8208e114b17e0e
brendon-ai <brendon-m@outlook.com>
2018-11-15 17:28:43 -0500

    Cleaned up unused stuff

--- pole_simulator.py
+++ pole_simulator.py
@@ -31,64 +31,45 @@ def get_pos_accel(angle, angle_speed, angle_accel, force):
 
 log = True
 
-p_range = 200
-d_range = 200
-target_angle = -0.3
-results = []
-parameters = []
-angles = []
 time_steps = 40_000
 p = 136
 d = 12
-positions = []
-# for pos_p in np.linspace(-0.01, 0, 200):
-for pos_p in [-0.02]:
-    for pos_d in [-0.1]:
-        angle = 0.1
-        angle_speed = 0
-        pos = 3
-        pos_speed = 0
-        force = 0
-        time = 0
-        e = []
-        f = []
-        for _ in range(time_steps):
-            target_angle = (pos_p * pos) + (pos_d * pos_speed)
-            p_error = angle - target_angle
-            d_error = angle_speed
-            e.append(abs(p_error))
-            force = (p_error * p) + (d_error * d)
-            angle_accel = get_angle_accel(angle, angle_speed, force)
-            pos_accel = get_pos_accel(angle, angle_speed, angle_accel, force)
-            angle += angle_speed * time_step
-            angles.append(angle)
-            angle_speed += (angle_accel * time_step)
-            pos += pos_speed * time_step
-            f.append(abs(pos))
-            pos_speed += pos_accel * time_step
-            if log:
-                print('Time:', time)
-                print('Force:', force)
-                print('Position:', pos)
-                print('Position Speed:', pos_speed)
-                print('Position Acceleration:', pos_accel)
-                print('Angle:', angle)
-                print('Angle Speed:', angle_speed)
-                print('Angle Acceleration:', angle_accel)
-                print()
-            if abs(pos) > track_limit:
-                if log:
-                    print('Failure')
-                break
-            time += time_step
-        if len(e) == time_steps:
-            results.append(np.mean(e))
-            positions.append(np.mean(f))
-            parameters.append((p, d, pos_p))
+pos_p = -0.02
+pos_d = -0.1
+angle = 0.1
+angle_speed = 0
+pos = 3
+pos_speed = 0
+time = 0
+abs_positions = []
+for _ in range(time_steps):
+    target_angle = (pos_p * pos) + (pos_d * pos_speed)
+    p_error = angle - target_angle
+    d_error = angle_speed
+    force = (p_error * p) + (d_error * d)
+    angle_accel = get_angle_accel(angle, angle_speed, force)
+    pos_accel = get_pos_accel(angle, angle_speed, angle_accel, force)
+    angle += angle_speed * time_step
+    angle_speed += (angle_accel * time_step)
+    pos += pos_speed * time_step
+    abs_positions.append(abs(pos))
+    pos_speed += pos_accel * time_step
+    if log:
+        print('Time:', time)
+        print('Force:', force)
+        print('Position:', pos)
+        print('Position Speed:', pos_speed)
+        print('Position Acceleration:', pos_accel)
+        print('Angle:', angle)
+        print('Angle Speed:', angle_speed)
+        print('Angle Acceleration:', angle_accel)
+        print()
+    if abs(pos) > track_limit:
+        if log:
+            print('Failure')
+        break
+    time += time_step
 
-print(min(positions))
-print(parameters[positions.index(min(positions))])
-# print(min(results))
-# print(parameters[results.index(min(results))])
-plt.plot(f)
+print(np.mean(abs_positions))
+plt.plot(abs_positions)
 plt.show()
