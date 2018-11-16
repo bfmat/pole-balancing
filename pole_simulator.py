d2c4c712f3514757d852ef6938510bdd35ed9e47
brendon-ai <brendon-m@outlook.com>
2018-11-15 19:48:06 -0500

    It works better, tuned parameters a bit

--- pole_simulator.py
+++ pole_simulator.py
@@ -11,7 +11,7 @@ cart_mass = 1
 pole_mass = 0.1
 pole_length = 0.5
 track_limit = 10
-failure_angle = 0.5  # radians
+failure_angle = 1  # radians
 time_step = 0.001
 
 
@@ -34,41 +34,41 @@ def get_pos_accel(angle, angle_speed, angle_accel, force):
 # State space control matrices (linearized)
 A = np.array([[1, 1, 0, 0], [gravity * pole_length, 1, 0, 0], [0, 0, 1, 1], [0, 0, 0, 1]])
 B = np.array([[0], [-1], [0], [1]])
-Q = 0.1 * np.diag([1 / (failure_angle ** 2), 1 / (0.3 ** 2), 1 / (track_limit ** 2), 1 / (5 ** 2)])
-R = np.array([[1 / (20 ** 2)]])
+Q = 0.001 * np.diag([1 / (0.03 ** 2), 1 / (0.01 ** 2), 1 / (20 ** 2), 1 / (1 ** 2)])
+R = np.array([[1 / (5 ** 2)]])
 # Calculate LQR optimal control policy
 K, _, _ = control.lqr(A, B, Q, R)
-# Example calculation
-x = -1 * np.array([[0.1], [0], [3], [0]])
-print(np.matmul(K, x))
 
-log = True
+log = False
 
-time_steps = 40_000
-p = 136
-d = 12
-pos_p = -0.02
-pos_d = -0.1
-angle = 0.1
+time_steps = 20_000
+angle = 0
 angle_speed = 0
 pos = 3
 pos_speed = 0
 time = 0
-abs_positions = []
+
+positions = []
+angles = []
+forces = []
+times = []
+
 for _ in range(time_steps):
-    target_angle = (pos_p * pos) + (pos_d * pos_speed)
-    p_error = angle - target_angle
-    d_error = angle_speed
     state = np.array([[angle], [angle_speed], [pos], [pos_speed]])
-    # force = (p_error * p) + (d_error * d)
     force = np.matmul(K, (state * -1))[0, 0]
     angle_accel = get_angle_accel(angle, angle_speed, force)
     pos_accel = get_pos_accel(angle, angle_speed, angle_accel, force)
     angle += angle_speed * time_step
     angle_speed += (angle_accel * time_step)
     pos += pos_speed * time_step
-    abs_positions.append(abs(pos))
     pos_speed += pos_accel * time_step
+    time += time_step
+
+    positions.append(pos)
+    angles.append(angle)
+    forces.append(force)
+    times.append(time)
+
     if log:
         print('Time:', time)
         print('Force:', force)
@@ -79,12 +79,21 @@ for _ in range(time_steps):
         print('Angle Speed:', angle_speed)
         print('Angle Acceleration:', angle_accel)
         print()
-    if abs(pos) > track_limit:
+    if abs(pos) >= track_limit or abs(angle) >= failure_angle:
         if log:
             print('Failure')
         break
-    time += time_step
 
-print(np.mean(abs_positions))
-plt.plot(abs_positions)
+plt.subplot(3, 1, 1)
+plt.plot(times, positions)
+plt.title('Position')
+
+plt.subplot(3, 1, 2)
+plt.plot(times, angles)
+plt.title('Angle')
+
+plt.subplot(3, 1, 3)
+plt.plot(times, forces)
+plt.title('Force Applied')
+
 plt.show()
