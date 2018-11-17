c90c232c7c20293c75711ad4b552d8523b55a220
brendon-ai <brendon-m@outlook.com>
2018-11-17 14:01:54 -0500

    Created GUI visualizer for pole balancing

--- pole_simulator.py
+++ pole_simulator.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
-import math
+import sys
 
 import control
 import matplotlib.pyplot as plt
 import numpy as np
+
+from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtGui import QPixmap, QImage, QPainter, QColor, QPen, QPalette, QFont
 
 gravity = 9.81
 cart_mass = 1
@@ -17,16 +21,16 @@ time_step = 0.001
 
 def get_angle_accel(angle, angle_speed, force):
     return (
-        ((gravity * math.sin(angle)) + (math.cos(angle) * (
-            (-force - (pole_mass * pole_length * (angle_speed ** 2) * math.sin(angle))) / (cart_mass + pole_mass)
+        ((gravity * np.sin(angle)) + (np.cos(angle) * (
+            (-force - (pole_mass * pole_length * (angle_speed ** 2) * np.sin(angle))) / (cart_mass + pole_mass)
         )))
-        / (pole_length * ((4/3) - ((pole_mass * (math.cos(angle) ** 2)) / (cart_mass + pole_mass))))
+        / (pole_length * ((4/3) - ((pole_mass * (np.cos(angle) ** 2)) / (cart_mass + pole_mass))))
     )
 
 
 def get_pos_accel(angle, angle_speed, angle_accel, force):
     return (
-        (force + (pole_mass * pole_length * (((angle_speed ** 2) * math.sin(angle)) + (angle_accel * math.cos(angle)))))
+        (force + (pole_mass * pole_length * (((angle_speed ** 2) * np.sin(angle)) + (angle_accel * np.cos(angle)))))
         / (cart_mass + pole_mass)
     )
 
@@ -34,17 +38,20 @@ def get_pos_accel(angle, angle_speed, angle_accel, force):
 # State space control matrices (linearized)
 A = np.array([[1, 1, 0, 0], [gravity / pole_length, 1, 0, 0], [0, 0, 1, 1], [0, 0, 0, 1]])
 B = np.array([[0], [-1], [0], [1]])
-Q = 0.001 * np.diag([1 / (0.1 ** 2), 1 / (0.1 ** 2), 1 / (20 ** 2), 1 / (1 ** 2)])
+Q = 0.01 * np.diag([1 / (0.1 ** 2), 1 / (0.1 ** 2), 1 / (20 ** 2), 1 / (1 ** 2)])
 R = np.array([[1 / (5 ** 2)]])
 # Calculate LQR optimal control policy
 K, _, _ = control.lqr(A, B, Q, R)
 
+# Switches for output modes
 log = False
+graph = False
+gui = True
 
-time_steps = 20_000
+time_steps = int(20 / time_step)
 angle = 0
 angle_speed = 0
-pos = 3
+pos = 9
 pos_speed = 0
 time = 0
 
@@ -84,16 +91,63 @@ for _ in range(time_steps):
             print('Failure')
         break
 
-plt.subplot(3, 1, 1)
-plt.plot(times, positions)
-plt.title('Position')
+if graph:
+    plt.subplot(3, 1, 1)
+    plt.plot(times, positions)
+    plt.title('Position')
 
-plt.subplot(3, 1, 2)
-plt.plot(times, angles)
-plt.title('Angle')
+    plt.subplot(3, 1, 2)
+    plt.plot(times, angles)
+    plt.title('Angle')
 
-plt.subplot(3, 1, 3)
-plt.plot(times, forces)
-plt.title('Force Applied')
+    plt.subplot(3, 1, 3)
+    plt.plot(times, forces)
+    plt.title('Force Applied')
 
-plt.show()
+    plt.show()
+
+
+class PoleSimulatorVisualizer(QWidget):
+
+    WIDTH = 2560
+    HEIGHT = 200
+    CART_POS_SCALE = WIDTH / (track_limit * 2)
+
+    def __init__(self):
+        super(PoleSimulatorVisualizer, self).__init__()
+        self.setFixedSize(self.WIDTH, self.HEIGHT)
+        self.show()
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update)
+        self.timer.start(time_step * 1000)
+        self.data_iterator = zip(positions, angles)
+        self.cart_pos = 0
+        self.pole_angle = 0
+
+    def update(self):
+        try:
+            self.cart_pos, self.pole_angle = next(self.data_iterator)
+        except StopIteration:
+            QApplication.quit()
+        self.repaint()
+
+    def paintEvent(self, _):
+        painter = QPainter()
+        painter.begin(self)
+        pen = QPen(Qt.black, 8)
+        painter.setPen(pen)
+        painter.drawLine(0, self.HEIGHT - 20, self.WIDTH, self.HEIGHT - 20)
+        pixel_pos = self.cart_pos * self.CART_POS_SCALE + (self.WIDTH // 2)
+        painter.drawRect(pixel_pos - 20, self.HEIGHT - (40 + 20), 40, 40)
+        pole_end_x = (np.sin(self.pole_angle) * pole_length * self.CART_POS_SCALE * 2) + pixel_pos
+        pole_end_y = (np.cos(self.pole_angle) * pole_length * self.CART_POS_SCALE * -2) + (self.HEIGHT - (20 + 30))
+        pen = QPen(Qt.red, 8)
+        painter.setPen(pen)
+        painter.drawLine(pixel_pos, self.HEIGHT - (20 + 30), pole_end_x, pole_end_y)
+        painter.end()
+
+
+if gui:
+    app = QApplication([])
+    ic = PoleSimulatorVisualizer()
+    sys.exit(app.exec_())
